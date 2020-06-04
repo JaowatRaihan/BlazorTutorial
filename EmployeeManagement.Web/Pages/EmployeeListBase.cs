06faf56a82f625bfd8bd535b96bc91f0b05ddf11
Jaowat Raihan <jaowatraihan@gmail.com>
2020-06-04 12:16:04 +0600

    First Commit

---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,72 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployees();
+            return base.OnInitializedAsync();
+        }
+
+        private void LoadEmployees()
+        {
+            Employee employee1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john@employee.com",
+                DateOfBirth = new DateTime(1980, 12, 5),
+                Department = new Department { DepartmentId = 1, DepartmentName = "HR" },
+                Gender = Gender.Male,
+                PhotoPath = "images/john.jpg"
+            };
+            Employee employee2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john@employee.com",
+                DateOfBirth = new DateTime(1980, 12, 5),
+                Department = new Department { DepartmentId = 1, DepartmentName = "HR" },
+                Gender = Gender.Male,
+                PhotoPath = "images/cris.jpg"
+            };
+            Employee employee3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john@employee.com",
+                DateOfBirth = new DateTime(1980, 12, 5),
+                Department = new Department { DepartmentId = 2, DepartmentName = "Officer" },
+                Gender = EmployeeManagement.Models.Gender.Male,
+                PhotoPath = "images/merry.jpg"
+            };
+            Employee employee4 = new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john@employee.com",
+                DateOfBirth = new DateTime(1980, 12, 5),
+                Department = new Department { DepartmentId = 2, DepartmentName = "Officer" },
+                Gender = EmployeeManagement.Models.Gender.Male,
+                PhotoPath = "images/alex.jpg"
+            };
+
+            Employees = new List<Employee> { employee1, employee2, employee3, employee4 };
+        }
+    }
+
+    
+}
